3420452ad7b2e4b5b2de5bc7e3a67b8b73a2f1f8
sandeep kamra <sandeep.d4d@gmail.com>
2025-11-06 14:12:17 +0530

    update backend path

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-// const PATH = process.env.MONGO_URI;
-const PATH = "mongodb+srv://sandeepkamara_circle:2LSF6s35c5VciEEu@circle.pgdamog.mongodb.net";
+const PATH = process.env.MONGO_URI;
+// const PATH = "mongodb+srv://sandeepkamara_circle:2LSF6s35c5VciEEu@circle.pgdamog.mongodb.net";
 // const PATH = "mongodb://localhost:27017/react-node-chat";
+
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(PATH);
